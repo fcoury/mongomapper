2097de2a5c7c6e376d8829c3290a9b74fb9b6da8
Felipe Coury <felipe.coury@gmail.com>
2009-07-19 21:25:35 -0300

    Allows polymorphic one to many for nested classes like Module1::Module2::ClassName

--- test/test_associations.rb
+++ test/test_associations.rb
@@ -66,12 +66,85 @@ class Catalog
   many :medias, :polymorphic => true
 end
 
+module TrModels
+  class Transport
+    include MongoMapper::EmbeddedDocument
+    key :license_plate, String
+  end
+  
+  class Car < TrModels::Transport
+    include MongoMapper::EmbeddedDocument
+    key :model, String
+    key :year, Integer
+  end
+  
+  class Bus < TrModels::Transport
+    include MongoMapper::EmbeddedDocument
+    key :max_passengers, Integer
+  end
+  
+  class Ambulance < TrModels::Transport
+    include MongoMapper::EmbeddedDocument
+    key :icu, Boolean
+  end
+  
+  class Fleet
+    include MongoMapper::Document
+    many :transports, :polymorphic => true, :class_name => "TrModels::Transport"
+    key :name, String
+  end
+end
+
 class AssociationsTest < Test::Unit::TestCase
   def setup
     Project.collection.clear
     Status.collection.clear
   end
   
+  context "Nested Polymorphic Many" do
+    should "default reader to empty array" do
+      fleet = TrModels::Fleet.new
+      fleet.transports.should == []
+    end
+
+    should "allow adding to association like it was an array" do
+      fleet = TrModels::Fleet.new
+      fleet.transports << TrModels::Car.new
+      fleet.transports.push TrModels::Bus.new
+      fleet.transports.size.should == 2
+    end
+
+    should "store the association" do
+      fleet = TrModels::Fleet.new
+      fleet.transports = [TrModels::Car.new("license_plate" => "DCU2013", "model" => "Honda Civic")]
+      fleet.save.should be_true
+
+      from_db = TrModels::Fleet.find(fleet.id)
+      from_db.transports.size.should == 1
+      from_db.transports[0].license_plate.should == "DCU2013"
+    end
+
+    should "store different associations" do
+      fleet = TrModels::Fleet.new
+      fleet.transports = [
+        TrModels::Car.new("license_plate" => "ABC1223", "model" => "Honda Civic", "year" => 2003),
+        TrModels::Bus.new("license_plate" => "XYZ9090", "max_passengers" => 51),
+        TrModels::Ambulance.new("license_plate" => "HDD3030", "icu" => true)
+      ]
+      fleet.save.should be_true
+
+      from_db = TrModels::Fleet.find(fleet.id)
+      from_db.transports.size.should == 3
+      from_db.transports[0].license_plate.should == "ABC1223"
+      from_db.transports[0].model.should == "Honda Civic"
+      from_db.transports[0].year.should == 2003
+      from_db.transports[1].license_plate.should == "XYZ9090"
+      from_db.transports[1].max_passengers.should == 51
+      from_db.transports[2].license_plate.should == "HDD3030"
+      from_db.transports[2].icu.should == true
+    end
+  end
+
   context "Polymorphic Many" do
     should "default reader to empty array" do
       catalog = Catalog.new
@@ -113,53 +186,6 @@ class AssociationsTest < Test::Unit::TestCase
       from_db.medias[2].file.should == "image.png"
       from_db.medias[2].width.should == 800
       from_db.medias[2].height.should == 600
-    end
-  end
-
-  context "Many embedded documents" do
-    should "default reader to empty array" do
-      project = Project.new
-      project.addresses.should == []
-    end
-
-    should "allow adding to association like it was an array" do
-      project = Project.new
-      project.addresses << Address.new
-      project.addresses.push Address.new
-      project.addresses.size.should == 2
-    end
-
-    should "be embedded in document on save" do
-      sb = Address.new(:city => 'South Bend', :state => 'IN')
-      chi = Address.new(:city => 'Chicago', :state => 'IL')
-      project = Project.new
-      project.addresses << sb
-      project.addresses << chi
-      project.save
-
-      from_db = Project.find(project.id)
-      from_db.addresses.size.should == 2
-      from_db.addresses[0].should == sb
-      from_db.addresses[1].should == chi
-    end
-
-    should "allow embedding arbitrarily deep" do
-      @document = Class.new do
-        include MongoMapper::Document
-        key :person, Person
-      end
-
-      meg = Person.new(:name => "Meg")
-      meg.child = Person.new(:name => "Steve")
-      meg.child.child = Person.new(:name => "Linda")
-
-      doc = @document.new(:person => meg)
-      doc.save
-
-      from_db = @document.find(doc.id)
-      from_db.person.name.should == 'Meg'
-      from_db.person.child.name.should == 'Steve'
-      from_db.person.child.child.name.should == 'Linda'
     end
   end
 
